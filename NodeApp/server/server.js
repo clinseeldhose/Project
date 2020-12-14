66f9aaf4cbfc4757e347b5f769dcb4213d62c943
THULASEEDHARAN <NDH00286@Nmcorp.Nissan.Biz>
2020-12-14 08:38:58 +0530

    fixed date format issue

--- NodeApp/server/server.js
+++ NodeApp/server/server.js
@@ -27,6 +27,7 @@ const cookieRoute = require('./routes/cookie');
 app.use('/api/user', userRoute);
 app.use('/api/cookie',cookieRoute);
 
+//console.log(Intl.DateTimeFormat().resolvedOptions().timeZone );
 
 app.listen(process.env.PORT || '3000', () => {
   console.log(`Server Running on port: ${process.env.PORT || '3000'}`);
